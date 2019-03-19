0f32c47780c6cc36f57d7dec4e40b63614bfbb96
dongbin <dongbin@fit2cloud.com>
2019-03-19 11:20:22 +0800

    fix

--- dump.sh
+++ dump.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-DB_USER=${MYSQL_ENV_DB_USER}
-DB_PASS=${MYSQL_ENV_DB_PASS}
-DB_HOST=${MYSQL_ENV_DB_HOST}
-DB_BACKUP_NAME=${MYSQL_ENV_DB_BACKUP_NAME}
 
-mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > /mysqldump/"${DB_BACKUP_NAME}".sql
-
-
+mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --single-transaction --skip-lock-tables --flush-privileges --all-databases | gzip > /mysqldump/"${DB_BACKUP_NAME}".tar.gz
 
--- import.sh
+++ import.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-DB_USER=${MYSQL_ENV_DB_USER}
-DB_PASS=${MYSQL_ENV_DB_PASS}
-DB_HOST=${MYSQL_ENV_DB_HOST}
-DB_BACKUP_NAME=${MYSQL_ENV_DB_BACKUP_NAME}
-
 mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" < /mysqldump/"${DB_BACKUP_NAME}".sql
 
 
